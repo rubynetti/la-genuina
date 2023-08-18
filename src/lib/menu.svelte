2bd1536e63b2871005fb66e8fcf67adbd0d59f09
Alessandro Descovi <descovi@gmail.com>
2023-08-18 16:08:09 +0200

    add menu

---
+++ src/lib/menu.svelte
@@ -0,0 +1,7 @@
+<div class="mb-5 text-center">
+  <a href="/" class="text-dark text-uppercase fw-bold">Home</a>
+  <div class="d-none">·</div>
+  <div class="d-none"><a href="/la-genuina" class="text-dark text-uppercase fw-bold">Archivio</a></div>
+  ·
+  <a href="/albo" class="text-dark text-uppercase fw-bold">Albo d'Oro</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <div class="container">
   <Header/>
-  <div class="mb-5 text-center">
-      <a href="/" class="text-dark text-uppercase fw-bold">Home</a>
-      <div class="d-none">·</div>
-      <div class="d-none"><a href="/la-genuina" class="text-dark text-uppercase fw-bold">Archivio</a></div>
-      ·
-      <a href="/albo" class="text-dark text-uppercase fw-bold">Albo d'Oro</a>
-  </div>
+  <Menu/>
   <div>
     <slot></slot>
   </div>
@@ -14,4 +8,5 @@
 
 <script>
   import Header from '$lib/header.svelte';
+  import Menu from '$lib/menu.svelte';
 </script>
