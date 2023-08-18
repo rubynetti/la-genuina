f4741d5da09974c890492465fe36e96f984e6048
Alessandro Descovi <descovi@gmail.com>
2023-08-18 13:53:28 +0200

    albo d'oro!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,18 @@
-<h1>La Genuina</h1>
-<ul>
-  <li>
-    <a href="/">Home</a>
-  <li>
-    <a href="/la-genuina">La Genuina</a>
-  </li>
-  <li>
-    <a href="/albo">Albo</a>
-  </li>
-</ul>
-<div>
-  <slot></slot>
+<div class="container">
+  <h1>La Genuina</h1>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    <li>
+      <a href="/la-genuina">Archivio</a>
+    </li>
+    <li>
+      <a href="/albo">Albo d'Oro</a>
+    </li>
+  </ul>
+  <div>
+    <slot></slot>
+  </div>
 </div>
 <script>
 export const prerender = true;
