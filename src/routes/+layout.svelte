66a1b18df75b1821363ba2262cf6f377963cb9df
Alessandro Descovi <descovi@gmail.com>
2023-08-18 13:47:12 +0200

    la genuina!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<h1>La Genuina</h1>
+<ul>
+  <li>
+    <a href="/">Home</a>
+  <li>
+    <a href="/la-genuina">La Genuina</a>
+  </li>
+  <li>
+    <a href="/albo">Albo</a>
+  </li>
+</ul>
+<div>
+  <slot></slot>
+</div>
+<script>
+export const prerender = true;
+</script>
---
+++ src/routes/la-genuina/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Archivio della Genuina</h1>
