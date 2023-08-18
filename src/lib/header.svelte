b43910eed1ae7061844e6e35450d158598a1e40c
Alessandro Descovi <descovi@gmail.com>
2023-08-18 16:07:28 +0200

    add first component

---
+++ src/lib/header.svelte
@@ -0,0 +1,4 @@
+<div class="header text-center my-5">
+  <h1 style="font-family: Ruwudu" class="my-0 fw-bold">La Genuina</h1>
+  <h2 style="font-family: Ruwudu" class="my-0 fs-5 fw-normal">Solo lacrime e lamenti 😭</h2>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <div class="container">
-  <div class="header text-center my-5">
-    <h1 style="font-family: Ruwudu" class="my-0 fw-bold">La Genuina</h1>
-    <h2 style="font-family: Ruwudu" class="my-0 fs-5 fw-normal">Solo lacrime e lamenti</h2>
-  </div>
+  <Header/>
   <div class="mb-5 text-center">
       <a href="/" class="text-dark text-uppercase fw-bold">Home</a>
       <div class="d-none">·</div>
@@ -14,3 +11,7 @@
     <slot></slot>
   </div>
 </div>
+
+<script>
+  import Header from '$lib/header.svelte';
+</script>
