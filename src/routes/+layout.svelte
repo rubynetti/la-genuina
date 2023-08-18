744290606ebfd38a1be00fc4857257c2ef14a28f
Alessandro Descovi <descovi@gmail.com>
2023-08-18 15:24:50 +0200

    add menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <div class="container">
-  <h1>La Genuina</h1>
-  <ul>
-    <li>
-      <a href="/">Home</a>
-    <li>
-      <a href="/la-genuina">Archivio</a>
-    </li>
-    <li>
-      <a href="/albo">Albo d'Oro</a>
-    </li>
-  </ul>
+  <div class="header text-center my-5">
+    <h1 style="font-family: Ruwudu" class="my-0 fw-bold">La Genuina</h1>
+    <h2 style="font-family: Ruwudu" class="my-0 fs-5 fw-normal">Tutte le lacrime del Fanta</h2>
+  </div>
+  <div>
+      <a href="/" class="text-dark text-uppercase fw-bold">Home</a>
+      ·
+      <a href="/la-genuina" class="text-dark text-uppercase fw-bold">Archivio</a>
+      ·
+      <a href="/albo" class="text-dark text-uppercase fw-bold">Albo d'Oro</a>
+  </div>
   <div>
     <slot></slot>
   </div>
