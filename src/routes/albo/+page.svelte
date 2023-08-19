a231b6f5ec62ed4087550189d8dfc92e78fc4e1e
Alessandro Descovi <descovi@gmail.com>
2023-08-19 17:36:32 +0200

    load dynamic data

--- src/routes/albo/+page.svelte
+++ src/routes/albo/+page.svelte
@@ -1,12 +1,12 @@
 <script>
   import Season from './season.svelte'
+  export let data;
 </script>
+
 <svelte:head>
   <title>La Genuina - Albo d'Oro</title>
 </svelte:head>
 
-<Season year="2022-23" winner="Matt" claim="Papere al comando" />
-<Season year="2021-22" winner="Benja" claim="Ho vinto ma non ricordo come" />
-<Season year="2020-21" winner="GP" claim="Povero Leo" />
-<Season year="2019-20" winner="GP" claim="Dove sono gli altri?" />
-<Season year="2018-19" winner="Ale" claim="Fatemi vedere le facce dei giocatori" />
+{#each data.seasons as season}
+  <Season {...season} />
+{/each}
