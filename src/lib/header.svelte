aaef4a2e1f503b81b424c43a1e84e69913b2633c
Alessandro Descovi <descovi@gmail.com>
2023-08-19 21:31:09 +0200

    lacrime ben spese!

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,4 +1,14 @@
 <div class="header text-center my-5">
   <h1 class="my-0 fw-bold ruwudu">La Genuina</h1>
-  <h2 class="my-0 fs-5 fw-normal ruwudu">😢 Solo lacrime e lamenti dal 2018 😭</h2>
+  <h2 class="my-0 fs-5 fw-normal ruwudu">😢 Lacrime e gemiti dal 2018 😭</h2>
 </div>
+
+<style>
+
+  @media screen and (max-width: 992px) {
+    h2 {
+      font-size: 0.9em !important
+    }
+
+  }
+</style>
