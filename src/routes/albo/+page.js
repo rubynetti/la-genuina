888158f66b0edc6f7bf1dd9f5d2064f896ef0c51
Alessandro Descovi <descovi@gmail.com>
2023-08-19 17:46:26 +0200

    load form json

--- src/routes/albo/+page.js
+++ src/routes/albo/+page.js
@@ -1,31 +1,4 @@
+import content from './content.json'
 export function load({ params }) {
-  return {
-    seasons: [
-      {
-        year: "2018-19",
-        winner: "Ale",
-        claim: "Fatemi vedere le facce dei giocatori"
-      },
-      {
-        year: "2019-20",
-        winner: "GP",
-        claim: "Dove sono gli altri?"
-      },
-      {
-        year: "2020-21",
-        winner: "GP",
-        claim: "Povero Leo"
-      },
-      {
-        year: "2021-22",
-        winner: "Benja",
-        claim: "Ho vinto ma non ricordo come"
-      },
-      {
-        year: "2022-23",
-        winner: "Matt",
-        claim: "Papere al comando e bonus difesa"
-      }
-    ]
-  };
+  return content;
 }
