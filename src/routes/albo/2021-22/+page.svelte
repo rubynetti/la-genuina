f62d80355d3ea5ab977df6f43986b68f49af626a
Alessandro Descovi <descovi@gmail.com>
2023-08-19 16:20:11 +0200

    layout al centro per tutti gli albi

--- src/routes/albo/2021-22/+page.svelte
+++ src/routes/albo/2021-22/+page.svelte
@@ -1,42 +1,34 @@
 <svelte:head>
   <title>2021-22 - Albo d'oro</title>
 </svelte:head>
+<h3>Classifica</h3>
 
-<div class="row">
-  <div class="col"></div>
-  <div class="col-lg-6">
-    <h3>Classifica</h3>
-    
-    <table class="mb-5">
-      <tr>
-        <td>1°</td>
-        <td>Benja</td>
-      </tr>
-      <tr>
-        <td>2°</td>
-        <td>Ale - Ueueggiare</td>
-      </tr>
-      <tr>
-        <td>3°</td>
-        <td>Adri - Santa Croce Bigolina</td>
-      </tr> 
-      <tr>
-        <td>4°</td>
-        <td>Leo - San Donato / Gp - Basetti per tutti</td>
-      </tr>
-      <tr>
-        <td>Presidente</td>
-        <td>Fra</td>
-      </tr>
-    </table>
-    
-    <h3>Ricordi</h3>
-    <p>Benja Campione del mondo.</p>
-    <p>
-      Ale campione d'inverno con una squadra orrenda ma incredibile (cmq
-      avevo Frattesi ancora sconosciuto tra i miei pupilli).
-    </p>
-  </div>
-  <div class="col"></div>
-</div>
-  
+<table class="mb-5">
+  <tr>
+    <td>1°</td>
+    <td>Benja</td>
+  </tr>
+  <tr>
+    <td>2°</td>
+    <td>Ale - Ueueggiare</td>
+  </tr>
+  <tr>
+    <td>3°</td>
+    <td>Adri - Santa Croce Bigolina</td>
+  </tr> 
+  <tr>
+    <td>4°</td>
+    <td>Leo - San Donato / Gp - Basetti per tutti</td>
+  </tr>
+  <tr>
+    <td>Presidente</td>
+    <td>Fra</td>
+  </tr>
+</table>
+
+<h3>Ricordi</h3>
+<p>Benja Campione del mondo.</p>
+<p>
+  Ale campione d'inverno con una squadra orrenda ma incredibile (cmq
+  avevo Frattesi ancora sconosciuto tra i miei pupilli).
+</p>
