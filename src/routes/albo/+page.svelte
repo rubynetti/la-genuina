b18c55bd333ff44fc7ede7283fcec8c331606494
Alessandro Descovi <descovi@gmail.com>
2023-08-19 18:36:45 +0200

    reverse fatto giusto

--- src/routes/albo/+page.svelte
+++ src/routes/albo/+page.svelte
@@ -1,12 +1,17 @@
 <script>
   import Season from './season.svelte'
   export let data;
+  // è necessario copiare l'array per poterlo invertire
+  // altrimenti si modificherebbe l'array originale
+  const seasons = [...data.seasons].reverse();
 </script>
 
 <svelte:head>
   <title>La Genuina - Albo d'Oro</title>
 </svelte:head>
 
-{#each [...data.seasons].reverse() as season}
-  <Season {...season} />
+{#each seasons as season(season.year)}
+  <Season year={season.year} 
+          claim={season.claim} 
+          winner={season.winner} />
 {/each}
