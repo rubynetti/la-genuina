e9c99a3243e846d51cc91f786cee9e57fe3c3f28
Alessandro Descovi <descovi@gmail.com>
2025-08-13 10:33:47 +0200

    refactor layout for improved readability

--- src/routes/albo/[slug]/+page.svelte
+++ src/routes/albo/[slug]/+page.svelte
@@ -7,37 +7,36 @@ export let data;
 </svelte:head>
 
 <div style="position: relative" class="text-center mb-5">
-<h2 class="my-5 d-inline">{data.year}</h2>
+  <h2 class="my-5 d-inline">{data.year}</h2>
 </div>
 
 <div class="mb-5">
-<h3 class="roboto-condensed">Classifica finale</h3>
-<ol class="p-0">
-  {#each data.rank as data}
-  <li>{data.mister} {#if data.team} · {data.team}{/if}</li>
-  {/each}
-</ol>
+  <h3 class="roboto-condensed">Classifica finale</h3>
+  <ol class="p-0">
+    {#each data.rank as data}
+    <li>{data.mister} {#if data.team} · {data.team}{/if}</li>
+    {/each}
+  </ol>
 </div>
 
 {#if data.sfiga != undefined}
 <div class="mb-5">
-<h3 class="roboto-condensed">Premio Sfiga</h3>
-<p>{data.sfiga?.mister} · {data.sfiga?.reason}</p>
+  <h3 class="roboto-condensed">Premio Sfiga</h3>
+  <p>{data.sfiga?.mister} · {data.sfiga?.reason}</p>
 </div>
 {/if}
 
 
 <div class="mb-5">
-<h3 class="roboto-condensed">Presidente</h3>
-<p>Fra</p>
+  <h3 class="roboto-condensed">Presidente</h3>
+  <p>Fra</p>
 </div>
 
 <div class="mb-5">
-
-<h3 class="roboto-condensed">Ricordi</h3>
-{#each data.memories as data}
-<p>{data}</p>
-{/each}
+  <h3 class="roboto-condensed">Ricordi</h3>
+  {#each data.memories as data}
+  <p>{data}</p>
+  {/each}
 </div>
 
 <style>
