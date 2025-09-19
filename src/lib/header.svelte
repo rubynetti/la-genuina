8a95aef80c2c21e232e40089a5c703e1e9eebee0
Alessandro Descovi <descovi@gmail.com>
2025-09-19 21:10:48 +0200

    refactor: restructure header to include link in title

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,7 @@
 <div class="header text-center my-5">
-  <h1 class="my-0 fw-bold ruwudu">La Genuina</h1>
+  <h1 class="my-0 fw-bold ruwudu">
+    <a href="/" class="text-decoration-none text-dark">La Genuina</a>
+  </h1>
   <h2 class="my-0 fs-5 fw-normal ruwudu">😢 Lacrime e gemiti dal 2018 😭</h2>
 </div>
 
